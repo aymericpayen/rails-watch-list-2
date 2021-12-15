db231b17b35b0b31a89de5758771b27ed1e6ec43
Aymeric Payen <aymeric.payen@gmail.com>
2021-12-15 18:03:04 +0100

    try

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -12,3 +12,17 @@ export default class extends Controller {
     });
   }
 }
+import { Controller } from "@hotwired/stimulus";
+import Typed from "typed.js";
+
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+      strings: ["Drama", "Classic", "Comedy", "Action", "Romantic", "Thriller", "Horror", "Slasher"],
+      typeSpeed: 40,
+      loop: true,
+      loopCount: Infinity,
+      showCursor: false
+    });
+  }
+}
